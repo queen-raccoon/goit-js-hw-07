8d978edb28cb42b766e2324346b504c2b5614777
queen-raccoon <k.i.d.v.s.k.a.t.2.1.3@gmail.com>
2023-09-14 13:08:33 +0400

    Summary

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,60 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const getListGallery = document.querySelector(".gallery");
 
+const renderList = (arr, container) => {
+    const markup = arr
+        .map(
+            (item) => 
+            `<li class="gallery__item js_gallery_item">
+                 <a class="gallery__link" href="${item.original}" onclick="return false;">
+                     <img 
+                        class="gallery__image"
+                        src="${item.preview}" 
+                        data-source="${item.preview}" 
+                        alt="${item.description}"
+                     >
+                 </a>
+            </li>`
+        ).join("");
+    
+    container.insertAdjacentHTML("beforeend", markup);
+}
+
+const changeSize = (e) => {
+    e.preventDefault();
+    if (e.currentTarget === e.target) {
+        return;
+    };
+
+    const currentListItem = e.target;
+    const currentListItemPreview = currentListItem.dataset.source;
+     const chosenImgSource = currentListItem.getAttribute("data-source");
+    const galleryItem = galleryItems.find((item) => item.preview === currentListItemPreview);
+
+    const modalInstance = basicLightbox.create(
+        `
+          <img class="gallery"
+            src="${galleryItem.original}" 
+            data-source="${galleryItem.original}"
+            alt="${galleryItem.description}"
+          >
+        `
+    );
+    modalInstance.show();
+
+    const escKey = (event) => {
+        if (event.key === "Escape") {
+            modalInstance.close();
+            getListGallery.removeEventListener("keydown", escKey);
+        };
+    };
+    getListGallery.addEventListener("keydown", escKey);
+};
+
+
+renderList(galleryItems, getListGallery);
+getListGallery.addEventListener("click", changeSize);
 console.log(galleryItems);
+
+
